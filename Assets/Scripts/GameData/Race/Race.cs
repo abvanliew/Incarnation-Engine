2e561e724319465d0fcd0b4d587407a34ddf295f
abvanliew <ab.vanliew@gmail.com>
2017-10-06 16:47:21 -0400

    Update Race.cs

--- Assets/Scripts/GameData/Race/Race.cs
+++ Assets/Scripts/GameData/Race/Race.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 namespace IncarnationEngine
 {
-    public class Race : MonoBehaviour
+    public class Race
     {
-        void Start()
-        {
-            
-        }
+        
     }
 }
