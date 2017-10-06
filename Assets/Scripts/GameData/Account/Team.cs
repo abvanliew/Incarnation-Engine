cc680bf30ac6e3ff6443cd45120b01c2d9c71011
abvanliew <ab.vanliew@gmail.com>
2017-10-06 16:46:52 -0400

    Update Team.cs

--- Assets/Scripts/GameData/Account/Team.cs
+++ Assets/Scripts/GameData/Account/Team.cs
@@ -6,9 +6,12 @@ namespace IncarnationEngine
 {
     public class Team
     {
-        void Start()
-        {
-            
-        }
+        string _Title;
+        double _Exp;
+        float _Gold;
+        float _Residium;
+        double _date;
+        Vector3 _Posistion;
+        int _Layer;
     }
 }
