f0edac7c58cc5eaea8ceb1b21626588b427f9de8
Brown, Aaron <BROWNA84@nationwide.com>
2018-12-29 03:35:57 -0500

    Initial Test of Attribute sliders
    Math and code seem to work, some wonkiness with the min and max distribution limits need to be ironed out

---
+++ Assets/Scripts/UI/Characters/CharacterAspectUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace IncarnationEngine
+{
+    public class CharacterAspectUI : MonoBehaviour
+    {
+        public Text Name;
+        public Text CurrentValue;
+        public Slider CurrentDistribution;
+        public Slider MinDistribution;
+        public Slider MaxDistribution;
+        public Text FinalValue;
+
+        [HideInInspector] public AspectGroup Parent;
+
+        public void DistributionChange()
+        {
+            Parent.UpdateCalculations();
+        }
+
+        public void SetAspectName( string name )
+        {
+            if( name != null )
+                Name.text = name;
+        }
+    }
+}
